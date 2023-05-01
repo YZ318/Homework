350a975661c425ad4d5cedbe7e08eb673b501427
YZ318 <YakovZhuravlev88@gmail.com>
2023-05-01 09:38:44 +0300

    Задание 19 выполнено

---
+++ HomeW19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int a = number / 10000;
+int b = (number / 1000) % 10;
+int c = (number / 100) % 10;
+int d = (number / 10) % 10;
+int e = number % 10;
+
+if (a == e && b == d)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
+
